cef5a6c8fbaa3aa7f0083529b216969931f37ede
Miles Granger <miles59923@gmail.com>
2019-12-01 08:49:12 +0100

    Impl Transformer for Pipeline

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(type_alias_impl_trait)]
 #![feature(associated_type_defaults)]
 
 pub mod minmax;
@@ -8,12 +7,7 @@ pub struct Pipeline<I, O> {
     steps: Vec<Box<dyn Transformer<I, O, Input = I, Output = O>>>,
 }
 
-impl<I, O> Pipeline<I, O>
-    where
-        Vec<I>: From<Vec<O>> + Clone,
-        Vec<O>: From<Vec<I>>,
-
-{
+impl<I, O> Pipeline<I, O> {
     pub fn new(steps: Vec<Box<dyn Transformer<I, O, Input = I, Output = O>>>) -> Self {
         Self { steps }
     }
@@ -21,19 +15,27 @@ impl<I, O> Pipeline<I, O>
     pub fn steps(&self) -> &Vec<Box<dyn Transformer<I, O, Input = I, Output = O>>> {
         &self.steps
     }
+}
 
-    pub fn fit(&mut self, x: Vec<I>) {
+impl<I, O> Transformer<I, O> for Pipeline<I, O>
+where
+    Vec<I>: From<Vec<O>> + Clone,
+    Vec<O>: From<Vec<I>>,
+{
+    type Input = I;
+    type Output = O;
+
+    fn fit(&mut self, x: Vec<I>) {
         self.steps.iter_mut().fold(x, |acc, transformer| {
             transformer.fit(acc.clone());
             transformer.transform(acc.into()).into()
         });
     }
-    pub fn transform(&self, x: Vec<I>) -> Vec<O> {
-        let out = self.steps
+    fn transform(&self, x: Vec<I>) -> Vec<O> {
+        let out = self
+            .steps
             .iter()
-            .fold(x, |out, transformer| {
-                transformer.transform(out).into()
-            });
+            .fold(x, |out, transformer| transformer.transform(out).into());
         out.into()
     }
 }
@@ -41,14 +43,6 @@ impl<I, O> Pipeline<I, O>
 pub trait Transformer<I, O> {
     type Input = I;
     type Output = O;
-    fn transform(&self, x: Vec<Self::Input>) -> Vec<Self::Output>;
     fn fit(&mut self, x: Vec<Self::Input>);
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    fn transform(&self, x: Vec<Self::Input>) -> Vec<Self::Output>;
 }
--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -1,4 +1,4 @@
-use pipes::{MinMaxScaler, Pipeline};
+use pipes::{MinMaxScaler, Pipeline, Transformer};
 
 #[test]
 fn test_pipeline_with_minmax() {
@@ -8,7 +8,27 @@ fn test_pipeline_with_minmax() {
     let x = vec![1, 2, 3, 4];
     pipe.fit(x);
 
-    // Ensure transforming new day min maxes
+    // Ensure transforming min & maxes
+    let x = vec![0, 1, 5];
+    let out = pipe.transform(x);
+    assert_eq!(out[0], 1);
+    assert_eq!(out[1], 1);
+    assert_eq!(out[2], 4);
+}
+
+#[test]
+fn test_pipeline_in_pipeline() {
+    let mut pipe = Pipeline::new(vec![
+        Box::new(Pipeline::new(vec![
+            Box::new(MinMaxScaler::new())
+        ]))
+    ]);
+
+    // Fit to some data
+    let x = vec![1, 2, 3, 4];
+    pipe.fit(x);
+
+    // Ensure transforming min & maxes
     let x = vec![0, 1, 5];
     let out = pipe.transform(x);
     assert_eq!(out[0], 1);
